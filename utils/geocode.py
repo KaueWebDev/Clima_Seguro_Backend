8dce25f048c0b4c5bc4f7a706a532bb58963c886
Kauê Anderson <164877319+KaueWebDev@users.noreply.github.com>
2025-11-20 01:26:56 -0300

    Update geocode.py

--- utils/geocode.py
+++ utils/geocode.py
@@ -1,6 +1,17 @@
 import requests
 
 def search_city(query):
-    url = f"https://nominatim.openstreetmap.org/search?q={query}&format=json&limit=5"
-    response = requests.get(url)
+    url = "https://nominatim.openstreetmap.org/search"
+    params = {
+        "q": query,
+        "format": "json",
+        "limit": 5
+    }
+
+    headers = {
+        "User-Agent": "weather-app-render/1.0 (contact: example@example.com)"
+    }
+
+    response = requests.get(url, params=params, headers=headers)
+
     return response.json()
