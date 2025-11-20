bbabbe29e645a3dbd5a398017f0247eaa0de1034
Kauê Anderson <164877319+KaueWebDev@users.noreply.github.com>
2025-11-20 01:31:53 -0300

    Update weather.py

--- utils/weather.py
+++ utils/weather.py
@@ -3,8 +3,8 @@ from config import OPENWEATHER_API_KEY
 
 def get_weather(lat, lon):
     url = (
-        f"https://api.openweathermap.org/data/2.5/weather?lat={lat}"
-        f"&lon={lon}&units=metric&appid={OPENWEATHER_API_KEY}&lang=pt_br"
+        f"https://api.openweathermap.org/data/2.5/weather"
+        f"?lat={lat}&lon={lon}&units=metric&appid={OPENWEATHER_API_KEY}&lang=pt_br"
     )
     response = requests.get(url)
-    return response.json() 
+    return response.json()
