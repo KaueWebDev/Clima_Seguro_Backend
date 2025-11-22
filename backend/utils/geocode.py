effa88a4e80dbd85a57b8fb517d85ab24027c914
Kauê Anderson <164877319+KaueWebDev@users.noreply.github.com>
2025-11-22 13:27:01 -0300

    Update geocode.py

--- backend/utils/geocode.py
+++ backend/utils/geocode.py
@@ -1,12 +1,17 @@
 import requests
 
 def search_city(query):
-    url = "https://nominatim.openstreetmap.org/search"
-    params = {
-        "q": query,
-        "format": "json",
-        "addressdetails": 1,
-        "limit": 5
-    }
-    res = requests.get(url, params=params)
-    return res.json()
+    try:
+        url = "https://nominatim.openstreetmap.org/search"
+        params = {
+            "q": query,
+            "format": "json",
+            "addressdetails": 1,
+            "limit": 5
+        }
+        res = requests.get(url, params=params, timeout=5)
+        res.raise_for_status()  # dispara exceção se HTTP != 200
+        return res.json()
+    except Exception as e:
+        print("Erro search_city:", e)
+        return []
